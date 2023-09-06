1d75d35bd4c6b2c0dda63bd4a318924a17d4a653
Diego Cerda <142626304+DiegoLacayo@users.noreply.github.com>
2023-09-06 15:26:06 -0600

    App.java

--- Icosaedro/src/App.java
+++ Icosaedro/src/App.java
@@ -7,19 +7,19 @@ public class App {
         // Donde A = Area.
         // En los casos: Alateral y Atotal
 
-        System.out.println("----Calculo de los datos de un Dodecaedro----");
+        System.out.println("----Calculo de los datos de un Icosaedro----");
 // --------------------------------------------------------------------------- //
-        System.out.println("Ingrese el Largo de las Aristas del Dodecaedro: ");
+        System.out.println("Ingrese el Largo de las Aristas del Icosaedro: ");
         Arista = lector.nextDouble();
 
         Atotal = 5 * Math.pow(Arista, 2) * (Math.sqrt(3));
         Alateral = Atotal / 20;
         Volumen = 5 * (3 + Math.sqrt(5)) * Math.pow(Arista, 3) / 12;
 
-        System.out.println("--Calculando AreaTotal, AreaLateral y Volumen del Dodecaedro--");
-        System.out.println("El Area Lateral del Dodecaedro es de: " + Alateral + " unidades cuadradas. ");
-        System.out.println("El Area Total del Dodecaedro es de: " + Atotal + " unidades cuadradas. ");      
-        System.out.println("El Volumen del Dodecaedro es de: " + Volumen + " unidades cubicas. ");
+        System.out.println("--Calculando AreaTotal, AreaLateral y Volumen del Icosaedro--");
+        System.out.println("El Area Lateral del Icosaedro es de: " + Alateral + " unidades cuadradas. ");
+        System.out.println("El Area Total del Icosaedro es de: " + Atotal + " unidades cuadradas. ");      
+        System.out.println("El Volumen del Icosaedro es de: " + Volumen + " unidades cubicas. ");
         lector.close();
     }
 }
